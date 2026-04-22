f7478cd51ef6db349ef2a9d8326805f6e04dbcf9
aishahc <57232038+aishahc@users.noreply.github.com>
2026-04-22 10:35:29 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -20,10 +20,10 @@ function createPhotoMap () {
   var map = L.map('map').setView([25.767, -80.216], 13);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-   var basemap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map), {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-)};
+	var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+  		maxZoom: 16,
+  		attribution: 'USGS'
+  	}).addTo(map);
 
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
   Papa.parse(csvUrl, {
